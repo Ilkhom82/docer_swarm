3ea7c8aa229f8a38dcea37123856b51994402216
Ilkhom82 <116688893+Ilkhom82@users.noreply.github.com>
2023-02-11 13:37:44 -0600

    Create visualizer.sh

---
+++ visualizer.sh
@@ -0,0 +1,7 @@
+docker service create \
+--constraint=node.role==manager \
+--mode=global \
+--publish mode=host,target=8080,published=8080 \
+--mount type=bind,src=/var/run/docker.sock,dst=/var/run/docker.sock \
+--name=visualizer \
+dockersamples/visualizer
